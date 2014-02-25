5aa8981109cbdc7f0776eeaa1c960ce4c518c4b6
mbragg02 <mikeabragg@icloud.com>
2014-02-25 15:33:29 +0000

    Factory updated

--- DFS/src/code/Factory.java
+++ DFS/src/code/Factory.java
@@ -1,11 +1,19 @@
 package code;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Factory {
 
 	private static Factory factory;
+	
+	
+	private List<DisplayALetter> leaves;
+	private List<CompositeDisplayALetter> nodes;
 
 	private Factory() {
-		// Empty private constructor
+		leaves = new ArrayList<DisplayALetter>();
+		nodes = new ArrayList<CompositeDisplayALetter>();
 	}
 
 	public static Factory getInstance() {
@@ -13,6 +21,14 @@ public class Factory {
 			factory = new Factory();
 		}
 		return factory;
+	}
+	
+	public void addNode(CompositeDisplayALetter node) {
+		this.nodes.add(node);
+	}
+	
+	public void addLeaves(DisplayALetter leaf) {
+		this.leaves.add(leaf);
 	}
 
 	public DisplayALetter getDisplayALetterA() {
@@ -29,6 +45,18 @@ public class Factory {
 	}
 	public CompositeDisplayALetter getCompositeDisplayALetter() {
 		return new CompositeDisplayALetter();
+	}
+	
+	
+	public void displayInOrderTree() {
+		nodes.get(0).add(nodes.get(1));
+		nodes.get(0).add(leaves.get(3));
+		nodes.get(1).add(nodes.get(2));
+		nodes.get(1).add(leaves.get(2));
+		nodes.get(2).add(leaves.get(0));
+		nodes.get(2).add(leaves.get(1));
+		
+		nodes.get(0).displayYourLetter();
 	}
 
 }
--- DFS/src/tests/LargerTreeStructureTest.java
+++ DFS/src/tests/LargerTreeStructureTest.java
@@ -17,23 +17,35 @@ public class LargerTreeStructureTest {
 	@Test
 	public void test() {
 		Factory fact = Factory .getInstance();
-		DisplayALetter a = fact.getDisplayALetterA();
-		DisplayALetter b = fact.getDisplayALetterB();
-		DisplayALetter c = fact.getDisplayALetterC();
-		DisplayALetter d = fact.getDisplayALetterD();
+		fact.addLeaves(fact.getDisplayALetterA());
+		fact.addLeaves(fact.getDisplayALetterB());
+		fact.addLeaves(fact.getDisplayALetterC());
+		fact.addLeaves(fact.getDisplayALetterD());
 		
-		CompositeDisplayALetter top = fact.getCompositeDisplayALetter();
-		CompositeDisplayALetter middle1 = fact.getCompositeDisplayALetter();
-		CompositeDisplayALetter middle2 = fact.getCompositeDisplayALetter();
+		fact.addNode(fact.getCompositeDisplayALetter());
+		fact.addNode(fact.getCompositeDisplayALetter());
+		fact.addNode(fact.getCompositeDisplayALetter());
 		
-		top.add(middle1);
-		top.add(d);
-		middle1.add(middle2);
-		middle1.add(c);
-		middle2.add(a);
-		middle2.add(b);
+//		DisplayALetter a = fact.getDisplayALetterA();
+//		DisplayALetter b = fact.getDisplayALetterB();
+//		DisplayALetter c = fact.getDisplayALetterC();
+//		DisplayALetter d = fact.getDisplayALetterD();
 		
-		top.displayYourLetter();
+//		CompositeDisplayALetter top     = fact.getCompositeDisplayALetter();
+//		CompositeDisplayALetter middle1 = fact.getCompositeDisplayALetter();
+//		CompositeDisplayALetter middle2 = fact.getCompositeDisplayALetter();
+		
+//		top.add(middle1);
+//		top.add(d);
+//		
+//		middle1.add(middle2);
+//		middle1.add(c);
+//		middle2.add(a);
+//		middle2.add(b);
+//		
+//		top.displayYourLetter();
+		
+		fact.displayInOrderTree();
 		
 	}
 
